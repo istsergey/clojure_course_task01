1b21559c735c2ccf18cf6f2ca6b123e77018fe17
istsergey <istsergey@gmail.com>
2013-03-19 07:30:15 +0300

    home task

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,28 +2,54 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
+(defn isClassL [v]
+  (and (map? v) (= (:class v) "l"))
+)
+
+(defn getHref [v]
+  (:href v)
+)
 
 (defn get-links []
-" 1) Find all elements containing {:class \"r\"}.
-
-Example:
-[:h3 {:class \"r\"} [:a {:shape \"rect\", :class \"l\",
-                         :href \"https://github.com/clojure/clojure\",
-                         :onmousedown \"return rwt(this,'','','','4','AFQjCNFlSngH8Q4cB8TMqb710dD6ZkDSJg','','0CFYQFjAD','','',event)\"}
-                     [:em {} \"clojure\"] \"/\" [:em {} \"clojure\"] \" · GitHub\"]]
-
-   2) Extract href from the element :a.
-
-The link from the example above is 'https://github.com/clojure/clojure'.
-
-  3) Return vector of all 10 links.
-
-Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
-"
+  ; необходимо достать href из элементов вида <a class="l"
   (let [data (parse "clojure_google.html")]
-    nil))
-
+    ; 1) разворачиваем все элементы в одну последовательность - "(flatten data)"
+    ; 2) Выбираем все maps содержащие класс "l"- "filter isClassL...."
+    ; 3) Получаем элементы href
+    (map getHref (filter isClassL (flatten data)))
+  ))
+  
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
